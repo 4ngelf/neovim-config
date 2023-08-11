b29bf15e9e8f7e5def808b6ffccd5342f01add14
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2023-08-11 02:08:05 -0400

    Change deprecated api & Add keybinding

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,21 +1,25 @@
 require "helpers/globals"
 require "helpers/keyboard"
 
-g.mapleader = ' '                                                                 -- Use Space, like key for alternative hotkeys
+g.mapleader = ' ' -- Use Space, like key for alternative hotkeys
 
 -- Normal mode {{{
-nm("J", "mzJ`z")        -- Join Lines without moving the cursor
-nm("<C-u>", "<C-u>zz")  -- Scroll up with cursor in the middle
-nm("<C-d>", "<C-d>zz")  -- Scroll down with cursor in the middle
-nm("n", "nzz")          -- Next find with cursor in the middle
-nm("N", "Nzz")          -- Previous find with cursor in the middle
+nm("J", "mzJ`z")           -- Join Lines without moving the cursor
+nm("<C-u>", "<C-u>zz")     -- Scroll up with cursor in the middle
+nm("<C-d>", "<C-d>zz")     -- Scroll down with cursor in the middle
+nm("n", "nzz")             -- Next find with cursor in the middle
+nm("N", "Nzz")             -- Previous find with cursor in the middle
 
-nm("<leader>d", "\"+d") -- delete forward with system clipboard
-nm("<leader>D", "\"+D") -- delete backward with system clipboard
-nm("<leader>p", "\"+p") -- paste forward with system clipboard
-nm("<leader>P", "\"+P") -- paste backward with system clipboard
-nm("<leader>y", "\"+y") -- Yank forward with system clipboard
-nm("<leader>Y", "\"+Y") -- Yank backward with system clipboard
+nm("<leader>d", "\"+d")    -- delete forward with system clipboard
+nm("<leader>D", "\"+D")    -- delete backward with system clipboard
+nm("<leader>p", "\"+p")    -- paste forward with system clipboard
+nm("<leader>P", "\"+P")    -- paste backward with system clipboard
+nm("<leader>y", "\"+y")    -- Yank forward with system clipboard
+nm("<leader>Y", "\"+Y")    -- Yank backward with system clipboard
+
+nm("<leader>F", function() -- Format buffer with Formatter
+  lsp.buf.format()
+end)
 -- }}}
 
 -- Visual mode {{{
@@ -24,19 +28,19 @@ vm("K", ":m '<-2<CR>gv") -- Move selected line Up
 -- }}}
 
 -- LSP {{{
-nm('K', '<cmd>lua vim.lsp.buf.hover()<CR>' )                                      -- Hover object
-nm('ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')                                -- Code actions
-nm('gR', '<cmd>lua vim.lsp.buf.rename()<CR>')                                     -- Rename an object
-nm('gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')                                -- Go to declaration
+nm('K', '<cmd>lua vim.lsp.buf.hover()<CR>')        -- Hover object
+nm('ga', '<cmd>lua vim.lsp.buf.code_action()<CR>') -- Code actions
+nm('gR', '<cmd>lua vim.lsp.buf.rename()<CR>')      -- Rename an object
+nm('gD', '<cmd>lua vim.lsp.buf.declaration()<cr>') -- Go to declaration
 -- }}}
 
 -- Telescope {{{
-nm('gd', '<cmd>Telescope lsp_definitions<CR>')                                   -- Goto declaration
-nm('<leader>ff', '<cmd>Telescope git_files<CR>')                                 -- Search for a file in project
-nm('<leader>fF', '<cmd>Telescope find_files<CR>')                                -- Search for a file (ignoring git-ignore)
-nm('<leader>fb', '<cmd>Telescope git_branches<CR>')                              -- Show git branches
-nm('<leader>fg', '<cmd>Telescope live_grep<CR>')                                 -- Find a string in project
-nm('<leader>a', '<cmd>Telescope<CR>')                                            -- Show all commands
+nm('gd', '<cmd>Telescope lsp_definitions<CR>')      -- Goto declaration
+nm('<leader>ff', '<cmd>Telescope git_files<CR>')    -- Search for a file in project
+nm('<leader>fF', '<cmd>Telescope find_files<CR>')   -- Search for a file (ignoring git-ignore)
+nm('<leader>fb', '<cmd>Telescope git_branches<CR>') -- Show git branches
+nm('<leader>fg', '<cmd>Telescope live_grep<CR>')    -- Find a string in project
+nm('<leader>a', '<cmd>Telescope<CR>')               -- Show all commands
 -- nm('<leader>p', '<cmd>Telescope oldfiles<CR>')                                -- Show recent files
 -- nm('<leader>i', '<cmd>Telescope jumplist<CR>')                                -- Show jumplist (previous locations)
 -- nm('<leader>q', '<cmd>Telescope buffers<CR>')                                 -- Show all buffers
@@ -44,15 +48,15 @@ nm('<leader>a', '<cmd>Telescope<CR>')                                           
 -- }}}
 
 -- Neogit {{{
-nm('<leader>ng', "<cmd>Neogit<CR>")     -- Open Neogit
+nm('<leader>ng', "<cmd>Neogit<CR>") -- Open Neogit
 -- }}}
 
 -- Trouble {{{
-nm('<leader>x', '<cmd>TroubleToggle<CR>')                                         -- Show all problems in project (with help of LSP)
-nm('gr', '<cmd>Trouble lsp_references<CR>')                                       -- Show use of object in project
+nm('<leader>x', '<cmd>TroubleToggle<CR>')   -- Show all problems in project (with help of LSP)
+nm('gr', '<cmd>Trouble lsp_references<CR>') -- Show use of object in project
 -- }}}
 
--- Neo Tree {{{ 
+-- Neo Tree {{{
 -- nm('<leader>v', '<cmd>NeoTreeFocusToggle<CR>')                                        -- Toggle file explorer
 -- }}}
 
