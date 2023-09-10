0fc1415f11296f302db3fd519f5d0a5f2a65f3b8
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2023-09-09 21:36:40 -0400

    refactorkeybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,11 +4,11 @@ require("helpers/keyboard")
 g.mapleader = " " -- Use Space, like key for alternative hotkeys
 
 -- Normal mode {{{
-nm("J", "mzJ`z") -- Join Lines without moving the cursor
+nm("J", "mzJ`z")       -- Join Lines without moving the cursor
 nm("<C-u>", "<C-u>zz") -- Scroll up with cursor in the middle
 nm("<C-d>", "<C-d>zz") -- Scroll down with cursor in the middle
-nm("n", "nzz") -- Next find with cursor in the middle
-nm("N", "Nzz") -- Previous find with cursor in the middle
+nm("n", "nzz")         -- Next find with cursor in the middle
+nm("N", "Nzz")         -- Previous find with cursor in the middle
 
 -- System clipboard {{{
 nm("<leader>d", '"+d') -- delete forward with system clipboard
@@ -20,21 +20,20 @@ nm("<leader>Y", '"+Y') -- Yank backward with system clipboard
 --- }}}
 
 -- Complex operations {{{
-nm("<leader>F", function() -- Format buffer with Formatter
-  lsp.buf.format()
-end)
+nm("<leader>F", lsp.buf.format) -- Format buffer with Formatter
 
+nm("<leader>s", lsp.buf.rename)  -- Substitute all references under cursor
 -- Sustitute all coincidences with word under cursor
-nm("<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+nm("<leader>S", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 nm("<leader>xx", "<cmd>silent !chmod +x %<CR>") -- Make current editing file executable
 nm("<leader>x", "<cmd>!%<CR>")                  -- Run current editing file
 
-nm("<leader>zo", "<cmd>%foldopen<CR>") -- Open all folds
-nm("<leader>zc", "<cmd>%foldclose<CR>") -- Close all folds
+nm("<leader>zo", "<cmd>%foldopen<CR>")          -- Open all folds
+nm("<leader>zc", "<cmd>%foldclose<CR>")         -- Close all folds
 
-nm("<leader>hl", function () -- Change treesitter highlighting 
-  local bufn = vim.fn.bufnr("%")
+nm("<leader>hl", function()                     -- Change treesitter highlighting
+  local bufn = vim.api.nvim_get_current_buf()
   local language = vim.fn.input("treesitter-highlighting: ")
   vim.treesitter.stop(bufn)
   vim.treesitter.start(bufn, language)
@@ -44,22 +43,22 @@ end)
 -- }}}
 
 -- Visual mode {{{
-vm('<leader>y', '"+y')   -- Copy selected text into system clipboard
+vm("<leader>y", '"+y')   -- Copy selected text into system clipboard
 
 vm("J", ":m '>+1<CR>gv") -- Move selected line Down
 vm("K", ":m '<-2<CR>gv") -- Move selected line Up
 -- }}}
 
 -- LSP {{{
-nm("K", "<cmd>lua vim.lsp.buf.hover()<CR>") -- Hover object
+nm("K", "<cmd>lua vim.lsp.buf.hover()<CR>")        -- Hover object
 nm("ga", "<cmd>lua vim.lsp.buf.code_action()<CR>") -- Code actions
-nm("gR", "<cmd>lua vim.lsp.buf.rename()<CR>") -- Rename an object
+nm("gR", "<cmd>lua vim.lsp.buf.rename()<CR>")      -- Rename an object
 nm("gD", "<cmd>lua vim.lsp.buf.declaration()<cr>") -- Go to declaration
 -- }}}
 
 -- Telescope {{{
 nm("gd", "<cmd>Telescope lsp_definitions<CR>") -- Goto declaration
-nm("<leader>f", function() -- Search for a file (ignoring .gitignore)
+nm("<leader>f", function()                     -- Search for a file (ignoring .gitignore)
   local items = fn.systemlist("fd -H -I -E '/.git' -tf")
   table.sort(items, function(a, b)
     return a > b
@@ -76,9 +75,9 @@ nm("<leader>ff", function() -- Search for a file in project (with .gitignore)
 end)
 
 nm("<leader>fb", "<cmd>Telescope git_branches<CR>") -- Show git branches
-nm("<leader>fg", "<cmd>Telescope live_grep<CR>") -- Find a string in project
-nm("<leader>fd", "<cmd>Telescope diagnostics<CR>") -- Show diagnostics
-nm("<leader>fa", "<cmd>Telescope<CR>") -- Show all commands
+nm("<leader>fg", "<cmd>Telescope live_grep<CR>")    -- Find a string in project
+nm("<leader>fd", "<cmd>Telescope diagnostics<CR>")  -- Show diagnostics
+nm("<leader>fa", "<cmd>Telescope<CR>")              -- Show all commands
 
 -- nm('<leader>p', '<cmd>Telescope oldfiles<CR>')                                -- Show recent files
 -- nm('<leader>i', '<cmd>Telescope jumplist<CR>')                                -- Show jumplist (previous locations)
