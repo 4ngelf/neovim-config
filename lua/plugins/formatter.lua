973afe52a1dc7bcc7cc371f3a6c2cdf89878a08d
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2023-11-09 00:51:44 -0400

    switch to format after save
    formatting on save is too slow with some LSPs and formatters.

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -26,15 +26,12 @@ return {
       require("conform").setup({
         formatters_by_ft = {
           lua = { "stylua" },
-          -- Conform will run multiple formatters sequentially
           python = { { "ruff_format", "black" } },
-          -- Use a sub-list to run only the first available formatter
           javascript = { { "prettierd", "prettier" } },
           ["*"] = { "codespell" },
         },
-        format_on_save = {
+        format_after_save = {
           -- These options will be passed to conform.format()
-          timeout_ms = 500,
           lsp_fallback = true,
         },
       })
