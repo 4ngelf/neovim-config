4965b259bcda99b8039c57bafd0b4990c2529117
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2023-11-19 16:36:43 -0400

    fix util functions

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -4,25 +4,28 @@ description: utilities commands
 
 local M = {}
 
+local function au(subname)
+  return vim.api.nvim_create_augroup("utility-" .. subname, { clear = true })
+end
+
 ---Lazily execute a command or lua function after {plugin} loads
 ---@param plugin string the name of the plugin set in Lazy configuration
 ---@param callback function() Lua function called after {plugin} loads.
 function M.lazy_execute(plugin, callback)
-  vim.api.nvim_create_autocmd("LazyLoad", {
-    group = "lazy-execute",
-    desc = "Lazy execute after "..plugin.." loads.",
-    callback = function (ev)
-      if ev.event ~= "LazyLoad" then return true end
+  vim.api.nvim_create_autocmd("User", {
+    group = au("lazy-execute"),
+    pattern = "LazyLoad",
+    desc = "Execute after " .. plugin .. " loads.",
+    callback = function(ev)
       if ev.data == plugin then
         callback()
+        return true
       end
-      return true
     end,
-    once = true
   })
 end
 
----Verify if a certain plugin is installed
+---Verify if a plugin is installed
 ---@param plugin string
 function M.has(plugin)
   return require("lazy.core.config").spec.plugins[plugin] ~= nil
