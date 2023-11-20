93ed1837046d452129f6ab0a1751680b77fb5c0e
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2023-11-20 18:35:20 -0400

    refactor util.lua

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -4,38 +4,39 @@ description: utilities commands
 
 local M = {}
 
-local function au(subname)
-  return vim.api.nvim_create_augroup("utility-" .. subname, { clear = false })
-end
-
----Lazily execute a command or lua function after {plugin} loads
----@param plugin string the name of the plugin set in Lazy configuration
----@param callback function Lua function called after {plugin} loads.
-function M.lazy_execute(plugin, callback)
-  vim.api.nvim_create_autocmd("User", {
-    group = au("lazy-execute"),
-    pattern = "LazyLoad",
-    desc = "Execute after " .. plugin .. " loads.",
-    callback = function(ev)
-      if ev.data == plugin then
-        callback()
-        return true
-      end
-    end,
-  })
-end
-
 ---Verify if a plugin is installed
 ---@param plugin string
 function M.has(plugin)
   return require("lazy.core.config").spec.plugins[plugin] ~= nil
 end
 
+---Lazily execute a callback if plugin is loaded
+---@param plugin_name string name of plugin set in Lazy configuration
+---@param callback fun(name: string) Lua function called if plugin is loaded
+--func function(name: string)
+function M.on_load(plugin_name, callback)
+  local Plugin = require("lazy.core.config").plugins[plugin_name]
+  if Plugin and Plugin._.loaded then
+    callback(plugin_name)
+  else
+    vim.api.nvim_create_autocmd("User", {
+      desc = "Execute after " .. plugin_name .. " loads.",
+      pattern = "LazyLoad",
+      callback = function(ev)
+        if ev.data == plugin_name then
+          callback(plugin_name)
+          return true
+        end
+      end,
+    })
+  end
+end
+
 ---Register mappings using which-key.nvim
 ---@param mappings table
 ---@param opts any
 function M.wk_register(mappings, opts)
-  M.lazy_execute("which-key.nvim", function()
+  M.on_load("which-key.nvim", function()
     require("which-key").register(mappings, opts)
   end)
 end
